d21492d114902fbdcd24f5ccc16bd2b9b95ca801
JONASPAULINO <jonaspaulinodasilva@gmail.com>
2023-05-21 14:38:46 -0300

    adicionar comentário

--- calcula.js
+++ calcula.js
@@ -2,9 +2,10 @@ function calcular(){
     let valor1 = parseFloat(document.getElementById('valor1').value);
     let valor2 = parseFloat(document.getElementById('valor2').value);
 
+    //verifica se está nulo
     if (!isNaN(valor1) && !isNaN(valor2)) {
         let total = valor1 * valor2;
-        alert('O total é: ' + total);
+        alert('O número: ' + valor1 + ' multiplicado por: '+ valor2 + ' é = ' + total);
     } else {
         alert('É preciso informar os dois valores');
     }
